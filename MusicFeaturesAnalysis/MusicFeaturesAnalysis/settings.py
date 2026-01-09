776e6bb03ac4a7dacfe6f3039203c2bdf6d2eeed
KarinaZalevska <s0beit72bt@gmail.com>
2026-01-09 12:15:33 +0200

    Connect to Postgre DB. Add .env

--- MusicFeaturesAnalysis/MusicFeaturesAnalysis/settings.py
+++ MusicFeaturesAnalysis/MusicFeaturesAnalysis/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
+#comment
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
