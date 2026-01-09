6e96b256970f96870aa909d3318a0ec75f3befb6
KarinaZalevska <s0beit72bt@gmail.com>
2026-01-09 12:43:51 +0200

    Add signin/login logic, templates.

--- MusicFeaturesAnalysis/mainapp/views.py
+++ MusicFeaturesAnalysis/mainapp/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import UserRegisterForm, UserLoginForm
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserLoginForm()
+    return render(request, 'mainapp/login.html', {'form': form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            messages.success(request, 'SignIn is success!')
+            return redirect('login')
+        else:
+            messages.error(request, 'SignIn is failed!')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'mainapp/signin.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('')
