8a69fba6f1bb08996cc3302b1b9e8e54217cab0f
Maksym Maryniuk <marinukmaks917@gmail.com>
2026-01-21 17:14:57 +0200

    Reinitialized model calls into function

--- MusicFeaturesAnalysis/DataModifying/models/loaded_model.py
+++ MusicFeaturesAnalysis/DataModifying/models/loaded_model.py
@@ -1,4 +1,13 @@
 import pickle
+from pathlib import Path
 
-with open("artifacts/genre_classifier.pkl", "rb") as f:
-    model = pickle.load(f)
+current_file = Path(__file__).resolve()
+
+models_dir = current_file.parent
+
+
+def rf_genre_classifier():
+    artifacts_dir = models_dir / "artifacts/genre_classifier.pkl"
+    with open(artifacts_dir, "rb") as f:
+        model = pickle.load(f)
+    return model
