0c1c51954f8dce8b13e9a1ecee43f1cba82bffff
Maksym Maryniuk <marinukmaks917@gmail.com>
2026-02-08 21:29:58 +0200

    Updating comments and docs

--- MusicFeaturesAnalysis/DataModifying/models/ModelRegistry.py
+++ MusicFeaturesAnalysis/DataModifying/models/ModelRegistry.py
@@ -1,4 +1,13 @@
 class ModelRegistry:
+    '''Special class to get/register models into your code.
+    If you want to take the model - try method get with this params:
+        "genre" - identifies subgenre
+        "user_profile" - return statistics based on user profile
+    If you want to register you model:
+    (ATTENTION - registered model should not be pathed any files beside
+    "DataModifying/models/artifacts/" to make code cleaner)
+        try register method with params "name" (str) and model (Classifier.py instance)
+    '''
     _models = {}
 
     @classmethod
