a462d4759eadb6d67e044af6eb5cf23236fea447
Pavlo <zgutapavlo@gmail.com>
2026-02-18 10:41:01 +0200

    Extracting genre from audio files

--- MusicFeaturesAnalysis/mainapp/utils/audio_utils.py
+++ MusicFeaturesAnalysis/mainapp/utils/audio_utils.py
@@ -4,6 +4,9 @@ from mainapp.services.reccobeatsapi.service import ReccoService
 from .search_utils import build_features_dict
 from .utils import file_hash
 from mainapp.services.models_crud.crud import audio_repo, song_repo, features_repo
+from DataModifying.models.Classifier import GenreClassifier
+
+classifier = GenreClassifier(model_path="DataModifying/models/artifacts/genre_classifier_v02.pkl")
 
 @transaction.atomic
 def create_song_f(user: User, file, title: str = "", artist: str = ""):
@@ -11,7 +14,13 @@ def create_song_f(user: User, file, title: str = "", artist: str = ""):
     if created:
         service = ReccoService()
         features_data = service.extract_from_audio(audio.file.path)
+        features_obj = type("Features", (), features_data)
+        genre = classifier.predict_subgenre(features_obj)
         features_repo.create_features(audio, build_features_dict(features_data))
     song, _ = song_repo.create_song(user, audio, title, artist)
+    
+    if genre:
+        song.genre = genre
+        song.save(update_fields=["genre"])
     
     return song
